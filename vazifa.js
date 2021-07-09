186d269c0fbc1c1ab98b06bc0705cbcbbb559da2
MuxtorovDM <86557961+MuxtorovDM@users.noreply.github.com>
2021-07-09 17:56:19 +0500

    Initial commit

---
+++ vazifa.js
@@ -0,0 +1,17 @@
+// var a = 1630;
+// console.log( a%2 !== 0  && `${a} toq son` || `${a} juft son` )
+
+
+
+// var number = "+998935761919";
+//     num = number.slice(0,6)+number.slice(-2).padStart(6,"*")
+// console.log(num)
+
+
+// var a = "WebBrain IT academy";
+//     b = a.split(" ")
+//     console.log(b[0]+' '+ b[2])
+    
+var x = "WebBrain IT academy IT";
+    y = x.replaceAll('IT', "")
+    console.log(y)
